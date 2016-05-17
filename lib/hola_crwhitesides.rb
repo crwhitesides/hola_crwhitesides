89bd97487493087caa9e0af6b8e9a0d1010fb4ef
Christian Whitesides <whitesidescr@gmail.com>
2016-05-17 12:34:21 -0600

    add moar code to root file and create hola_crwhitesides dir and translator.rb file

--- lib/hola_crwhitesides.rb
+++ lib/hola_crwhitesides.rb
@@ -1,5 +1,21 @@
 class Hola
-  def self.hi
-    puts "Hello world!"
+  def self.hi(language = "english")
+    translator = Translator.new(language)
+    translator.hi
+  end
+end
+
+class Hola::Translator
+  def initialize(language)
+    @language = language
+  end  
+
+  def hi
+    case @language
+    when"spanish"
+      "Hola mundo!"
+    else
+      "Hello world!"
+    end
   end
 end
